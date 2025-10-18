5e44ee26b15644e5d755570d771ec813c73779fd
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2025-10-18 12:35:16 +0530

    Added Login System
    Implemented Login System
    -> Backend
     - Routing for Auths like register, login, logout, password reset, OTP code and resend OTP codes
    -> Frontend
    - Sign In and Sign Out page is completed
    - Password reset and forget password pages should be tested and modified.
    - OTP page is completed
    - Need to work on Dashboard and initial Page
    
    -> Validation
    - Mostly handled at backend
    
    -> Theme controller and User data context are added as services.
    -> Using Cookies for token and stores theme data in local storage.
    -> MongoDB is used for database.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,48 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { AuthProvider } from '../contexts/AuthContext';
+
+import AuthLayout from './pages/common/auth/AuthLayout';
+import Login from './pages/common/auth/pages/SignIn';
+import Register from './pages/common/auth/pages/SignUp';
+import OTPVerification from './pages/common/auth/pages/OTPVerification';
+import ForgotPassword from './pages/common/auth/pages/ForgotPassword';
+import ResetPassword from './pages/common/auth/pages/ResetPassword';
+
+import ProtectedRoute from './components/ProtectedRoute';
+
+import MainLayout from './pages/user/MainLayout';
+import Dashboard from "./pages/user/dashboard/Dashboard";
+import FocusSession from './pages/user/FocusSession';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/verify-email" element={<OTPVerification />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/" element={<Navigate to="/login" replace />} />
+            </Route>
+
+            <Route element={
+              <ProtectedRoute>
+                <MainLayout />
+              </ProtectedRoute>
+            }>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/focus-page" element={<FocusSession />} />
+            </Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
