4d12dab02930131f6c9ab98eab47656ea4c29c09
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2025-10-25 19:10:36 +0530

    Added Role based system
    Major Update: Added Role Based System
    Minor Update: Improve Auto login and Dashboard for normal users.
    
    Auth Context
    - Added new function to check auth whenever the app loads ( for auto login)
    
    Admin Service
    - Added services function for class, CRUD function for users, get all users  and get sessions for all users
    
    App
    - To implement role based login I have improved the structure. Moved children logic inside Protected Route
    
    ProtectedRoute
    - Role based Layout allocation logic is implemented.
    
    AdminMainLayout & UserMainLayout
    - Removed HeaderNav for admin. Most of the code is same for both
    
    Dashboard
    - Improved Dashboard for Users
    - Implemented Dashboard for admins
    
    Need to do:
    - Theme and other settings to admin
    - Sound Effects for Users Focus

--- frontend/src/pages/common/auth/AuthLayout.jsx
+++ frontend/src/pages/common/auth/AuthLayout.jsx
@@ -1,7 +1,10 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import ThemeToggle from '../../../components/ThemeToggle/ThemeToggle';
+import { useAuth } from '../../../../contexts/AuthContext';
 
 const AuthLayout = () => {
+  const {user} = useAuth()
+  if(user?._id) return <Navigate to="/dashboard" replace />
   return (
     <div
       className="min-h-screen"
